034f4746c208e1d07d6e20261acd69d7837d4d6d
lucas araujo <lucasao744@gmail.com>
2024-07-31 16:36:12 -0300

    mostrar todos os estados dos cards

--- src/components/FlashCard/FlashCard.js
+++ src/components/FlashCard/FlashCard.js
@@ -5,13 +5,53 @@ import {
     BotaoResposta
 } from "./StylesFlashCard";
 import play from "../../assets/seta_play.png";
+import turnedCard from "../../assets/seta_virar.png";
+import { useState } from 'react';
 
-export default function FlashCard({card, index}) {
+export default function FlashCard({ card, index }) {
+    const [Started, setStarted] = useState(false);
+    const [turned, setTurned] = useState(false);
+    const [finished, setFinished] = useState(false);
 
+    function showQuestion() {
+        if(!finished){
+            setStarted(true)
+        }
+    }
+
+    function answerquestion() {
+            setStarted(false)
+            setFinished(true)
+    }
     return (
-        <PerguntaFechada>
-            <p> Pergunta {index + 1}</p>
-            <img src={play} alt="Icon" />
-        </PerguntaFechada>
+        <>
+            {!Started ? (
+                <PerguntaFechada>
+                    <p> Pergunta {index + 1}</p>
+                    <img src={play} alt="Icon" onClick={showQuestion}/>
+                </PerguntaFechada>
+            ) : (
+                <PerguntaAberta>
+                    {!turned ? (
+                        <>
+                            {card.question}
+                            <img src={turnedCard} alt="icon de virar o card" onClick={()=> setTurned(true)}/>
+                        </>
+                    ) : (
+                        <>
+                            {card.answer}
+                            <ContainerBotoes>
+                                <button onClick={answerquestion}>Não lembrei</button>
+                                <button onClick={answerquestion}> Quase não lembrei</button>
+                                <button onClick={answerquestion}>Zap</button>
+                            </ContainerBotoes>
+                        </>
+                    )}
+                </PerguntaAberta>
+            )}
+        </>
+
+
+
     );
 }
